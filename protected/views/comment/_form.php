2613958f3210975a4fcd1c6e7f491df9faa9d839
Almir Neto <almirneto338@gmail.com>
2021-01-12 11:01:44 -0300

    generated CRUD for all models

---
+++ protected/views/comment/_form.php
@@ -0,0 +1,48 @@
+<?php
+/* @var $this CommentController */
+/* @var $model Comment */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'comment-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'post_id'); ?>
+		<?php echo $form->textField($model,'post_id',array('size'=>10,'maxlength'=>10)); ?>
+		<?php echo $form->error($model,'post_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'commentary'); ?>
+		<?php echo $form->textField($model,'commentary',array('size'=>60,'maxlength'=>150)); ?>
+		<?php echo $form->error($model,'commentary'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'author'); ?>
+		<?php echo $form->textField($model,'author',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'author'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'date'); ?>
+		<?php echo $form->textField($model,'date'); ?>
+		<?php echo $form->error($model,'date'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/views/comment/_view.php
@@ -0,0 +1,29 @@
+<?php
+/* @var $this CommentController */
+/* @var $data Comment */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('post_id')); ?>:</b>
+	<?php echo CHtml::encode($data->post_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('commentary')); ?>:</b>
+	<?php echo CHtml::encode($data->commentary); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('author')); ?>:</b>
+	<?php echo CHtml::encode($data->author); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('date')); ?>:</b>
+	<?php echo CHtml::encode($data->date); ?>
+	<br />
+
+
+</div>
