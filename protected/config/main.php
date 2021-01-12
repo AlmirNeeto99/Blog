460eccac077e2afb8cd9fd52256ce2a4ec668f69
Almir Neto <almirneto338@gmail.com>
2021-01-12 11:12:28 -0300

    changing app name to match the requirements

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Conexa Blog',
+	'name'=>'Blog Conexa',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
