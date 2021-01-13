1acfe3eeb3ff78c5db6b1f46fc2ca6fe038a7583
Almir Neto <almirneto338@gmail.com>
2021-01-13 16:21:57 -0300

    formatação do arquivo

--- protected/views/post/_search.php
+++ protected/views/post/_search.php
@@ -6,40 +6,40 @@
 
 <div class="wide form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'action'=>Yii::app()->createUrl($this->route),
-	'method'=>'get',
-)); ?>
+	<?php $form = $this->beginWidget('CActiveForm', array(
+		'action' => Yii::app()->createUrl($this->route),
+		'method' => 'get',
+	)); ?>
 
 	<div class="row">
-		<?php echo $form->label($model,'id'); ?>
-		<?php echo $form->textField($model,'id',array('size'=>10,'maxlength'=>10)); ?>
+		<?php echo $form->label($model, 'id'); ?>
+		<?php echo $form->textField($model, 'id', array('size' => 10, 'maxlength' => 10)); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->label($model,'categoria_id'); ?>
-		<?php echo $form->textField($model,'categoria_id',array('size'=>10,'maxlength'=>10)); ?>
+		<?php echo $form->label($model, 'categoria_id'); ?>
+		<?php echo $form->textField($model, 'categoria_id', array('size' => 10, 'maxlength' => 10)); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->label($model,'conteudo'); ?>
-		<?php echo $form->textArea($model,'conteudo',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->label($model, 'conteudo'); ?>
+		<?php echo $form->textArea($model, 'conteudo', array('rows' => 6, 'cols' => 50)); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->label($model,'autor'); ?>
-		<?php echo $form->textField($model,'autor',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->label($model, 'autor'); ?>
+		<?php echo $form->textField($model, 'autor', array('size' => 60, 'maxlength' => 255)); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->label($model,'data'); ?>
-		<?php echo $form->textField($model,'data'); ?>
+		<?php echo $form->label($model, 'data'); ?>
+		<?php echo $form->textField($model, 'data'); ?>
 	</div>
 
 	<div class="row buttons">
 		<?php echo CHtml::submitButton('Search'); ?>
 	</div>
 
-<?php $this->endWidget(); ?>
+	<?php $this->endWidget(); ?>
 
 </div><!-- search-form -->
