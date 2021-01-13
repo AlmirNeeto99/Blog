6844097e25652e718967c401ecbdca6d647cdc64
Almir Neto <almirneto338@gmail.com>
2021-01-13 14:31:25 -0300

    traduzindo arquivo sql

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,34 +1,36 @@
-CREATE DATABASE `blog` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `blog` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `blog`;
-
 /*Creating categories table*/
-CREATE TABLE IF NOT EXISTS `category`(
+CREATE TABLE IF NOT EXISTS `categoria`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(255) NOT NULL,
+    `nome` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 /*Inserting default categories into their tables*/
-INSERT INTO `category`(`title`) VALUES("Financeiro"), ("Integrações"),("Serviços"),("Agenda"),("Parceiros"),("Outros");
-
+INSERT INTO `categoria`(`nome`)
+VALUES("Financeiro"),
+    ("Integrações"),
+    ("Serviços"),
+    ("Agenda"),
+    ("Parceiros"),
+    ("Outros");
 /*Creating posts table*/
 CREATE TABLE IF NOT EXISTS `post`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `category_id` INT UNSIGNED NOT NULL,
-    `content` TINYTEXT NOT NULL,
-    `author` VARCHAR(255) NOT NULL,
-    `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    
+    `categoria_id` INT UNSIGNED NOT NULL,
+    `conteudo` TINYTEXT NOT NULL,
+    `autor` VARCHAR(255) NOT NULL,
+    `data` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`category_id`) REFERENCES `category`(`id`)
+    FOREIGN KEY (`categoria_id`) REFERENCES `categoria`(`id`)
 ) ENGINE = InnoDB;
-
 /*Creating posts comments table*/
-CREATE TABLE IF NOT EXISTS `comment` (
+CREATE TABLE IF NOT EXISTS `comentario` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `post_id` INT UNSIGNED NOT NULL,
-    `commentary` VARCHAR(150) NOT NULL,
-    `author` VARCHAR(255) NOT NULL,
-    `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `texto` VARCHAR(150) NOT NULL,
+    `autor` VARCHAR(255) NOT NULL,
+    `data` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`post_id`) REFERENCES `post`(`id`)
 ) ENGINE = InnoDB;
