d13c9d815fe25224b0e9319017a4e9f61a148498
Almir Neto <almirneto338@gmail.com>
2021-01-13 14:47:39 -0300

    adicionando # no id do post

--- protected/views/post/_view.php
+++ protected/views/post/_view.php
@@ -9,7 +9,7 @@
 				<b>
 					<?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:
 				</b>
-				<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id' => $data->id)); ?>
+				<?php echo CHtml::link(CHtml::encode("#" . $data->id), array('view', 'id' => $data->id)); ?>
 			</li>
 			<li>
 				<b>
