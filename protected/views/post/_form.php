0ce359c9e7102ddaf969cd6a2f138023552db733
Almir Neto <almirneto338@gmail.com>
2021-01-13 15:06:48 -0300

    melhorando visualização do form do post

--- protected/views/post/_form.php
+++ protected/views/post/_form.php
@@ -6,43 +6,37 @@
 
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'post-form',
-	'enableAjaxValidation'=>false,
-)); ?>
+	<?php $form = $this->beginWidget('CActiveForm', array(
+		'id' => 'post-form',
+		'enableAjaxValidation' => false,
+	)); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<p class="note">Campos com <span class="required">*</span> são obrigatórios.</p>
 
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'categoria_id'); ?>
-		<?php echo $form->textField($model,'categoria_id',array('size'=>10,'maxlength'=>10)); ?>
-		<?php echo $form->error($model,'categoria_id'); ?>
+		<?php echo $form->labelEx($model, 'categoria_id'); ?>
+		<?php echo $form->textField($model, 'categoria_id', array('size' => 10, 'maxlength' => 10, 'class' => 'form-control')); ?>
+		<?php echo $form->error($model, 'categoria_id'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'conteudo'); ?>
-		<?php echo $form->textArea($model,'conteudo',array('rows'=>6, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'conteudo'); ?>
+		<?php echo $form->labelEx($model, 'conteudo'); ?>
+		<?php echo $form->textArea($model, 'conteudo', array('rows' => 6, 'cols' => 50, 'class' => 'form-control')); ?>
+		<?php echo $form->error($model, 'conteudo'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'autor'); ?>
-		<?php echo $form->textField($model,'autor',array('size'=>60,'maxlength'=>255)); ?>
-		<?php echo $form->error($model,'autor'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'data'); ?>
-		<?php echo $form->textField($model,'data'); ?>
-		<?php echo $form->error($model,'data'); ?>
+		<?php echo $form->labelEx($model, 'autor'); ?>
+		<?php echo $form->textField($model, 'autor', array('size' => 60, 'maxlength' => 255, 'class' => 'form-control')); ?>
+		<?php echo $form->error($model, 'autor'); ?>
 	</div>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Cadastrar' : 'Salvar', array('class' => 'btn btn-outline-conexa')); ?>
 	</div>
 
-<?php $this->endWidget(); ?>
+	<?php $this->endWidget(); ?>
 
 </div><!-- form -->
