cf2d26909d59b75cbd6d21aa5805f0d3ac3df771
Almir Neto <almirneto338@gmail.com>
2021-01-13 11:50:00 -0300

    post listing

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,20 +1,64 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name;
+$this->pageTitle = Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<main class="my-4">
+	<div class="row">
+		<div class="col-12">
+			<h1 class="text-center">
+				<?php echo CHtml::encode(Yii::app()->name); ?>
+			</h1>
+		</div>
+		<div class="col-12 col-md-6 border-bottom border-success mx-auto my-3"></div>
+	</div>
+	<div class="row">
+		<div class="col-12">
+			<h2 class="text-center">
+				Veja aqui os posts mais recentes
+			</h2>
+		</div>
+		<?php if (empty($posts)) : ?>
+			<div class="alert alert-danger my-4">
+				<h3>
+					Oops... Nenhum post encontrado.
+				</h3>
+				<p>
+					Tente novamente mais tarde.
+				</p>
+			</div>
+		<?php else : ?>
+			<div class="col-12">
+				<div class="row">
+					<?php foreach ($posts as $post) : ?>
+						<div class="col-12 col-md-4">
+							<div class="card">
+								<div class="card-header">
+									<h4>
+										Post #<?= $post->id; ?>
+									</h4>
+								</div>
+								<div class="card-body">
+									<div class="card-content">
+										<?= $post->content; ?>
+									</div>
+								</div>
+								<div class="card-footer">
+									<p>
+										Por: <strong><?= $post->author; ?></strong>
+									</p>
+									<p class="mb-0">
+										<small>
+											Em: <?= date("d/m/Y H:i", strtotime($post->date)); ?>
+										</small>
+									</p>
+								</div>
+							</div>
+						</div>
+					<?php endforeach; ?>
+				</div>
+			</div>
+		<?php endif; ?>
+	</div>
+</main>
