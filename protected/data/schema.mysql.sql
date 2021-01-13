bec69d8249e7f7db53b25928a5aae49824899f88
Almir Neto <almirneto338@gmail.com>
2021-01-13 12:32:44 -0300

    sql adjusts

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,14 +1,17 @@
+CREATE DATABASE `blog` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `blog`;
+
 /*Creating categories table*/
-CREATE TABLE IF NOT EXISTS `categories`(
+CREATE TABLE IF NOT EXISTS `category`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 /*Inserting default categories into their tables*/
-INSERT INTO `categories`(`title`) VALUES("Financeiro"), ("Integrações"),("Serviços"),("Agenda"),("Parceiros"),("Outros");
+INSERT INTO `category`(`title`) VALUES("Financeiro"), ("Integrações"),("Serviços"),("Agenda"),("Parceiros"),("Outros");
 
 /*Creating posts table*/
-CREATE TABLE IF NOT EXISTS `posts`(
+CREATE TABLE IF NOT EXISTS `post`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `category_id` INT UNSIGNED NOT NULL,
     `content` TINYTEXT NOT NULL,
@@ -16,16 +19,16 @@ CREATE TABLE IF NOT EXISTS `posts`(
     `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`)
+    FOREIGN KEY (`category_id`) REFERENCES `category`(`id`)
 ) ENGINE = InnoDB;
 
 /*Creating posts comments table*/
-CREATE TABLE IF NOT EXISTS `comments` (
+CREATE TABLE IF NOT EXISTS `comment` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `post_id` INT UNSIGNED NOT NULL,
     `commentary` VARCHAR(150) NOT NULL,
     `author` VARCHAR(255) NOT NULL,
     `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`post_id`) REFERENCES `posts`(`id`)
+    FOREIGN KEY (`post_id`) REFERENCES `post`(`id`)
 ) ENGINE = InnoDB;
