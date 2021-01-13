5060676dca6702076eeb366c5cbfdf5e982af605
Almir Neto <almirneto338@gmail.com>
2021-01-13 16:22:17 -0300

    melhorando layout da listagem de posts + sombra

--- protected/views/post/_view.php
+++ protected/views/post/_view.php
@@ -2,37 +2,39 @@
 /* @var $this PostController */
 /* @var $data Post */
 ?>
-<div class="card my-1">
-	<div class="card-body">
-		<ul class="list-unstyled">
-			<li>
-				<b>
-					<?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:
-				</b>
-				<?php echo CHtml::link(CHtml::encode("#" . $data->id), array('view', 'id' => $data->id)); ?>
-			</li>
-			<li>
-				<b>
-					<?php echo CHtml::encode($data->getAttributeLabel('categoria_nome')); ?>:
-				</b>
-				<?php echo CHtml::encode($data->categoria->nome); ?>
+<div class="col-12 col-md-6">
+	<div class="card my-2 shadow">
+		<div class="card-body">
+			<ul class="list-unstyled">
+				<li>
+					<b>
+						<?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:
+					</b>
+					<?php echo CHtml::link(CHtml::encode("#" . $data->id), array('view', 'id' => $data->id)); ?>
+				</li>
+				<li>
+					<b>
+						<?php echo CHtml::encode($data->getAttributeLabel('categoria_nome')); ?>:
+					</b>
+					<?php echo CHtml::encode($data->categoria->nome); ?>
 
-			</li>
-			<li>
-				<b>
-					<?php echo CHtml::encode($data->getAttributeLabel('conteudo')); ?>:
-				</b>
-				<?php echo CHtml::encode($data->conteudo); ?>
-			</li>
-			<li>
-				<b>
-					<?php echo CHtml::encode($data->getAttributeLabel('autor')); ?>:
-				</b>
-				<?php echo CHtml::encode($data->autor); ?>
-			</li>
-		</ul>
-	</div>
-	<div class="card-footer">
-		Em: <?php echo date("d/m/Y H:i", strtotime($data->data)); ?>
+				</li>
+				<li>
+					<b>
+						<?php echo CHtml::encode($data->getAttributeLabel('conteudo')); ?>:
+					</b>
+					<?php echo CHtml::encode($data->conteudo); ?>
+				</li>
+				<li>
+					<b>
+						<?php echo CHtml::encode($data->getAttributeLabel('autor')); ?>:
+					</b>
+					<?php echo CHtml::encode($data->autor); ?>
+				</li>
+			</ul>
+		</div>
+		<div class="card-footer">
+			Em: <?php echo date("d/m/Y H:i", strtotime($data->data)); ?>
+		</div>
 	</div>
 </div>
