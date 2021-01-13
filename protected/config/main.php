5e8873ea98c3780fc3bfe2b14504df9d09110202
Almir Neto <almirneto338@gmail.com>
2021-01-13 10:20:38 -0300

    fixing giix path

--- protected/config/main.php
+++ protected/config/main.php
@@ -6,20 +6,20 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Blog Conexa',
+	'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+	'name' => 'Blog Conexa',
 
 	// preloading 'log' component
-	'preload'=>array('log'),
+	'preload' => array('log'),
 
 	// autoloading model and component classes
-	'import'=>array(
+	'import' => array(
 		'application.models.*',
 		'application.components.*',
-		'ext.giix.giix-components.*', // giix components
+		'ext.giix-components.*', // giix components
 	),
 
-	'modules'=>array(
+	'modules' => array(
 		// uncomment the following to enable the Gii tool
 		/*
 		'gii'=>array(
@@ -32,18 +32,18 @@ return array(
 		'gii' => array(
 			'class' => 'system.gii.GiiModule',
 			'generatorPaths' => array(
-				'ext.giix.giix-core', // giix generators
+				'ext.giix-core', // giix generators
 			),
 			'password' => 'root',
-			'ipFilters'=>array('127.0.0.1','::1')
+			'ipFilters' => array('127.0.0.1', '::1')
 		),
 	),
 
 	// application components
-	'components'=>array(
-		'user'=>array(
+	'components' => array(
+		'user' => array(
 			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
+			'allowAutoLogin' => true,
 		),
 		// uncomment the following to enable URLs in path-format
 		/*
@@ -60,25 +60,25 @@ return array(
 		// 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		// ),
 		// uncomment the following to use a MySQL database
-		
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=blog',
+
+		'db' => array(
+			'connectionString' => 'mysql:host=127.0.0.1;dbname=blog',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => 'root',
 			'charset' => 'utf8',
 		),
-		
-		'errorHandler'=>array(
+
+		'errorHandler' => array(
 			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
+			'errorAction' => 'site/error',
 		),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
+		'log' => array(
+			'class' => 'CLogRouter',
+			'routes' => array(
 				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
+					'class' => 'CFileLogRoute',
+					'levels' => 'error, warning',
 				),
 				// uncomment the following to show log messages on web pages
 				/*
@@ -92,8 +92,8 @@ return array(
 
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
-	'params'=>array(
+	'params' => array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail' => 'webmaster@example.com',
 	),
 );
