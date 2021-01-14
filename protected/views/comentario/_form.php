86f32a354e948c4a5a15ff97ce17c0176106c1f2
Almir Neto <almirneto338@gmail.com>
2021-01-14 14:15:06 -0300

    ajuste no form de comentario

--- protected/views/comentario/_form.php
+++ protected/views/comentario/_form.php
@@ -6,43 +6,43 @@
 
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'comentario-form',
-	'enableAjaxValidation'=>false,
-)); ?>
+	<?php $form = $this->beginWidget('CActiveForm', array(
+		'id' => 'comentario-form',
+		'enableAjaxValidation' => false,
+		"method" => 'post',
+		'action' => Yii::app()->createUrl("comentario/create")
+	)); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<p class="note">Campos com <span class="required">*</span> são obrigatórios.</p>
 
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'post_id'); ?>
-		<?php echo $form->textField($model,'post_id',array('size'=>10,'maxlength'=>10)); ?>
-		<?php echo $form->error($model,'post_id'); ?>
+		<h5 class="text-danger">
+			Você está comentando no post #<?= $post ?>
+		</h5>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'texto'); ?>
-		<?php echo $form->textField($model,'texto',array('size'=>60,'maxlength'=>150)); ?>
-		<?php echo $form->error($model,'texto'); ?>
+		<?php echo $form->hiddenField($model, 'post_id', array('size' => 10, 'maxlength' => 10, 'value' => $post)); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'autor'); ?>
-		<?php echo $form->textField($model,'autor',array('size'=>60,'maxlength'=>255)); ?>
-		<?php echo $form->error($model,'autor'); ?>
+		<?php echo $form->labelEx($model, 'texto'); ?>
+		<?php echo $form->textField($model, 'texto', array('size' => 60, 'maxlength' => 150, 'class' => 'form-control')); ?>
+		<?php echo $form->error($model, 'texto'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'data'); ?>
-		<?php echo $form->textField($model,'data'); ?>
-		<?php echo $form->error($model,'data'); ?>
+		<?php echo $form->labelEx($model, 'autor'); ?>
+		<?php echo $form->textField($model, 'autor', array('size' => 60, 'maxlength' => 255, 'class' => 'form-control')); ?>
+		<?php echo $form->error($model, 'autor'); ?>
 	</div>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Comentar' : 'Salvar', array('class' => 'btn btn-outline-conexa')); ?>
 	</div>
 
-<?php $this->endWidget(); ?>
+	<?php $this->endWidget(); ?>
 
 </div><!-- form -->
