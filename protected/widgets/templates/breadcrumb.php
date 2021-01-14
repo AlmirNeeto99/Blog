e1a39a57bc6e0fbf4b7a892dbe3a3da840d47786
Almir Neto <almirneto338@gmail.com>
2021-01-14 12:00:43 -0300

    mudando cor dos links do breadcrumb

--- protected/widgets/templates/breadcrumb.php
+++ protected/widgets/templates/breadcrumb.php
@@ -1,6 +1,6 @@
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb justify-content-end mr-0">
-        <li class="breadcrumb-item"><a href="/">Home</a></li>
+        <li class="breadcrumb-item"><a class="text-conexa" href="/">Home</a></li>
         <?php $last = false;
         $index = 0;
         foreach ($links as $key => $value) {
@@ -8,7 +8,7 @@
             if ($last) { ?>
                 <li class="breadcrumb-item active" aria-current="page"><?= $value ?></li>
             <?php } else { ?>
-                <li class="breadcrumb-item"><a href="<?= Yii::app()->createUrl($value[0]) ?>"><?= $key ?></a></li>
+                <li class="breadcrumb-item"><a class="text-conexa" href="<?= Yii::app()->createUrl($value[0]) ?>"><?= $key ?></a></li>
         <?php }
             $index++;
         } ?>
