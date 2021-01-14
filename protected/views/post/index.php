2c000a358437d7274f0ff496b3429341a48d2d1a
Almir Neto <almirneto338@gmail.com>
2021-01-14 16:22:38 -0300

    removido menu nao utilizado

--- protected/views/post/index.php
+++ protected/views/post/index.php
@@ -5,11 +5,6 @@
 $this->breadcrumbs = array(
 	'Posts',
 );
-
-$this->menu = array(
-	array('label' => 'Create Post', 'url' => array('create')),
-	array('label' => 'Manage Post', 'url' => array('admin')),
-);
 ?>
 
 <h1>Posts</h1>
