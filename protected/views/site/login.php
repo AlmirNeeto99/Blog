c73daf509b162d75e3e02e22b55234df6253eb3a
Almir Neto <almirneto338@gmail.com>
2021-01-14 17:18:33 -0300

    melhoria na tela de login

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -3,51 +3,60 @@
 /* @var $model LoginForm */
 /* @var $form CActiveForm  */
 
-$this->pageTitle=Yii::app()->name . ' - Login';
-$this->breadcrumbs=array(
-	'Login',
-);
+$this->pageTitle = Yii::app()->name . ' - Login';
 ?>
+<div class="row">
+	<div class="col-12 col-md-6 mx-auto">
+		<h1 class="text-center">Login</h1>
+		<?php $this->widget("Divider") ?>
 
-<h1>Login</h1>
-
-<p>Please fill out the following form with your login credentials:</p>
-
-<div class="form">
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'login-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
-)); ?>
-
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
+		<p>Preencha os campos abaixo com seus dados de login:</p>
 	</div>
+</div>
+<div class="row">
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-		<p class="hint">
-			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-		</p>
+	<div class="col-12 col-md-6 mx-auto">
+
+		<div class="form">
+			<?php $form = $this->beginWidget('CActiveForm', array(
+				'id' => 'login-form',
+				'enableClientValidation' => true,
+				'clientOptions' => array(
+					'validateOnSubmit' => true,
+				),
+			)); ?>
+
+			<p class="note">Campos com <span class="required">*</span> são obrigatórios.</p>
+
+			<div class="row">
+				<?php echo $form->labelEx($model, 'username'); ?>
+				<?php echo $form->textField($model, 'username', array("class" => 'form-control')); ?>
+				<?php echo $form->error($model, 'username'); ?>
+			</div>
+
+			<div class="row">
+
+				<?php echo $form->labelEx($model, 'password'); ?>
+				<?php echo $form->passwordField($model, 'password', array("class" => 'form-control')); ?>
+				<?php echo $form->error($model, 'password'); ?>
+			</div>
+
+			<div class="row rememberMe">
+				<div class="custom-control custom-checkbox">
+					<?php echo $form->checkBox($model, 'rememberMe', array("class" => 'custom-control-input')); ?>
+					<?php echo $form->label($model, 'rememberMe', array("class" => "custom-control-label")); ?>
+				</div>
+
+				<?php echo $form->error($model, 'rememberMe'); ?>
+			</div>
+
+			<div class="row buttons">
+				<div class="col-12 text-center">
+					<?php echo CHtml::submitButton('Login', array("class" => "btn btn-outline-conexa")); ?>
+				</div>
+			</div>
+
+			<?php $this->endWidget(); ?>
+		</div><!-- form -->
 	</div>
-
-	<div class="row rememberMe">
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Login'); ?>
-	</div>
-
-<?php $this->endWidget(); ?>
-</div><!-- form -->
+</div>
