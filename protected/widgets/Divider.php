72c9bf70c2db5f51fb90b6b71fe9d6072ec168ef
Almir Neto <almirneto338@gmail.com>
2021-01-14 00:56:59 -0300

    divider melhorado

--- protected/widgets/Divider.php
+++ protected/widgets/Divider.php
@@ -2,9 +2,19 @@
 
 class Divider extends CWidget
 {
+    public $size = null;
+    public $color = "conexa";
     public function init()
     {
-        echo "<div class='w-100 bg-conexa my-2' style='height:1px;'></div>";
+        switch ($this->size) {
+            case "small":
+                $this->size = "w-25";
+                break;
+            case "large":
+                $this->size = "w-100";
+                break;
+        }
+        echo "<div class='$this->size bg-$this->color my-2' style='height:1px;'></div>";
     }
 
     public function run()
