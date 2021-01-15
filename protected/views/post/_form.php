2952f3115736b8a5d0bc6242557a32cc757302c0
Almir Neto <almirneto338@gmail.com>
2021-01-15 10:53:07 -0300

    ajuste de exibição do formulario

--- protected/views/post/_form.php
+++ protected/views/post/_form.php
@@ -16,31 +16,44 @@
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model, 'categoria_id'); ?>
-		<?php echo $form->dropDownList($model, 'categoria_id', $categorias, array("class" => "form-control")) ?>
-		<?php echo $form->error($model, 'categoria_id'); ?>
+		<div class="col-12">
+			<?php echo $form->labelEx($model, 'categoria_id'); ?>
+			<?php echo $form->dropDownList($model, 'categoria_id', $categorias, array("class" => "form-control")) ?>
+			<?php echo $form->error($model, 'categoria_id'); ?>
+		</div>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model, 'titulo'); ?>
-		<?php echo $form->textField($model, 'titulo', array('size' => 60, 'maxlength' => 255, 'class' => 'form-control')); ?>
-		<?php echo $form->error($model, 'titulo'); ?>
+		<div class="col-12">
+			<?php echo $form->labelEx($model, 'titulo'); ?>
+			<?php echo $form->textField($model, 'titulo', array('size' => 60, 'maxlength' => 255, 'class' => 'form-control')); ?>
+			<?php echo $form->error($model, 'titulo'); ?>
+		</div>
+
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model, 'conteudo'); ?>
-		<?php echo $form->textArea($model, 'conteudo', array('rows' => 6, 'cols' => 50, 'class' => 'form-control')); ?>
-		<?php echo $form->error($model, 'conteudo'); ?>
+		<div class="col-12">
+			<?php echo $form->labelEx($model, 'conteudo'); ?>
+			<?php echo $form->textArea($model, 'conteudo', array('rows' => 6, 'cols' => 50, 'class' => 'form-control')); ?>
+			<?php echo $form->error($model, 'conteudo'); ?>
+		</div>
+
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model, 'autor'); ?>
-		<?php echo $form->textField($model, 'autor', array('size' => 60, 'maxlength' => 255, 'class' => 'form-control')); ?>
-		<?php echo $form->error($model, 'autor'); ?>
+		<div class="col-12">
+			<?php echo $form->labelEx($model, 'autor'); ?>
+			<?php echo $form->textField($model, 'autor', array('size' => 60, 'maxlength' => 255, 'class' => 'form-control')); ?>
+			<?php echo $form->error($model, 'autor'); ?>
+		</div>
+
 	</div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Cadastrar' : 'Salvar', array('class' => 'btn btn-outline-conexa')); ?>
+	<div class="row">
+		<div class="col-12 my-1">
+			<?php echo CHtml::submitButton($model->isNewRecord ? 'Cadastrar' : 'Salvar', array('class' => 'btn btn-outline-conexa')); ?>
+		</div>
 	</div>
 
 	<?php $this->endWidget(); ?>
