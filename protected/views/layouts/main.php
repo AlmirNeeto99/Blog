ebffdf6dc63896f08655639ff54e903abfcd9499
Almir Neto <almirneto338@gmail.com>
2021-01-15 10:52:56 -0300

    adicionando dropdown de navegacao no topo correcao de tamanho do container no navbar

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,20 +5,15 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
-
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/logo.png" type="image/png">
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
 	<!--[if lt IE 8]>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
 	<![endif]-->
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/app.css" />
-	<!-- <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" /> -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Blinker:wght@100;200;300;400;600;700;800;900&display=swap" rel="stylesheet">
@@ -26,47 +21,62 @@
 </head>
 
 <body>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<div class="container-md">
+	<nav class="navbar navbar-expand navbar-light bg-light">
+		<div class="container px-3">
 			<a class="navbar-brand" href="/">
 				<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/conexa.png" alt="Conexa Blog">
 			</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<div class="dropdown">
+						<button class="btn btn-outline-conexa dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<i class="fa fa-cog"></i>
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="/">
 
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav mr-auto">
-					<!-- <li class="nav-item active">
-					<a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-				</li> -->
-				</ul>
-				<!-- <form class="form-inline my-2 my-lg-0">
-				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-			</form> -->
-			</div>
+								<i class="fa fa-home mr-1"></i>
+
+								Home
+							</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="<?= Yii::app()->createUrl("post/create") ?>">
+								<i class="fa fa-plus mr-1"></i>
+								Cadastre seu post
+							</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="<?= Yii::app()->createUrl("post/index") ?>">
+								<i class="fa fa-list-ul mr-1"></i>
+								Posts
+							</a>
+						</div>
+					</div>
+				</li>
+			</ul>
 		</div>
-
 	</nav>
-	<div class="container min-vh-100 my-3" id="page">
-		<div class="row justify-content-end">
-			<div class="col-12 col-md-5">
-				<?php if (isset($this->breadcrumbs) && !empty($this->breadcrumbs)) : ?>
+	<header class="container my-3">
+		<?php if (isset($this->breadcrumbs) && !empty($this->breadcrumbs)) : ?>
+			<div class="row justify-content-end">
+				<div class="col-12 col-md-5">
 					<?php $this->widget('Breadcrumb', array(
 						'links' => $this->breadcrumbs,
 					)); ?>
 					<!-- breadcrumbs -->
-				<?php endif ?>
+				</div>
+			</div>
+		<?php endif ?>
+	</header>
+	<main class="min-vh-100 my-3">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<?php echo $content; ?>
+				</div>
 			</div>
 		</div>
 
-
-		<?php echo $content; ?>
-
-		<div class="clear"></div>
-
-	</div><!-- page -->
+	</main>
 	<footer class="bg-conexa text-white text-center py-2">
 		Copyright &copy; <?php echo date('Y'); ?> by Conexa.<br />
 		All Rights Reserved.<br />
